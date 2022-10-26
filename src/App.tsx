e85eba9f8d513937f5532092e57105ecb186dc62
don <donpark@pm.me>
2022-10-26 01:35:19 -0500

    useDapp & initial component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { useEtherBalance, useEthers } from '@usedapp/core';
+import { MetamaskConnect } from './components/MetamaskConnect';
+
 function App() {
+  const { account, deactivate, chainId } = useEthers();
+  const etherBalance = useEtherBalance(account);
+  console.log(etherBalance);
+
   return (
     <div className="App">
-     hi.......
+      hi...
+      <MetamaskConnect />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Mainnet, DAppProvider, Config, Goerli } from '@usedapp/core';
+import { getDefaultProvider, providers } from 'ethers';
+
+// const config: Config = {
+//     readOnlyChainId: Mainnet.chainId,
+//     readOnlyUrls: {
+//         [Mainnet.chainId]: getDefaultProvider('mainnet'),
+//         [Goerli.chainId]: getDefaultProvider('goerli'),
+//     },
+// }
+
+// https://chainlist.org/chain/1
+const rpc_url =
+  'https://eth-mainnet.nodereal.io/v1/1659dfb40aa24bbb8153a677b98064d7';
+const customHttpProvider = new providers.JsonRpcProvider(rpc_url);
+customHttpProvider.getBlockNumber().then((result) => {
+  console.log('Current block number: ' + result);
+});
+
+//https://usedapp-docs.netlify.app/docs/Guides/Connecting/Read-only
+const config: Config = {
+  readOnlyChainId: Mainnet.chainId,
+  readOnlyUrls: {
+    [Mainnet.chainId]: customHttpProvider,
+  },
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <DAppProvider config={config}>
     <App />
-  </React.StrictMode>
+  </DAppProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
